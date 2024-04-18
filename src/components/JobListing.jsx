6f70eedcf4df3cff3e101ef518874eed8529da87
Mihir More <mihirmore.25@gmail.com>
2024-04-18 13:47:02 +0530

    Add Description Toggle and Location Icon complete

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,5 +1,16 @@
+import { useState } from "react";
+import { FaMapMarker } from "react-icons/fa";
+
 const JobListing = ({ job }) => {
-    console.log(`Job ---> `, job);
+    // console.log(`Job ---> `, job);
+    const [showFullDescription, setShowFullDescription] = useState(false);
+
+    let description = job.description;
+
+    if (!showFullDescription) {
+        description = description.substring(0, 90) + "...";
+    }
+
     return (
         <>
             <div
@@ -12,7 +23,16 @@ const JobListing = ({ job }) => {
                         <h3 className="text-xl font-bold">{job.title}</h3>
                     </div>
 
-                    <div className="mb-5">{job.description}</div>
+                    <div className="mb-5">{description}</div>
+
+                    <button
+                        className="text-indigo-500 mb-5 hover:text-indigo-600 underline"
+                        onClick={() =>
+                            setShowFullDescription((prevState) => !prevState)
+                        }
+                    >
+                        {showFullDescription ? "Less" : "More"}
+                    </button>
 
                     <h3 className="text-indigo-500 mb-2">
                         {job.salary} / Year
@@ -22,7 +42,7 @@ const JobListing = ({ job }) => {
 
                     <div className="flex flex-col lg:flex-row justify-between mb-4">
                         <div className="text-orange-700 mb-3">
-                            <i className="fa-solid fa-location-dot text-lg"></i>
+                            <FaMapMarker className="inline mb-1 mr-1"/>
                             {job.location}
                         </div>
                         <a
