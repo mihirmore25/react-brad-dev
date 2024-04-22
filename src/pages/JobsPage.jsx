78bd7dcd4d6be2cc36700284f9085de7785fea94
Mihir More <mihirmore.25@gmail.com>
2024-04-22 12:25:20 +0530

    Add Jobs to JobsPage component

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,7 +1,13 @@
-const JobsPage = () => {
-  return (
-    <div>JobsPage</div>
-  )
-}
+import JobListings from "../components/JobListings";
 
-export default JobsPage
+const JobsPage = () => {
+    return (
+        <>
+            <section className="bg-blue-50 px-4 py-6">
+                <JobListings />
+            </section>
+        </>
+    );
+};
+
+export default JobsPage;
